244faa30bf242df111fba70ea3989da581016a2c
Johnny Robeson <johnny@localmomentum.net>
2025-01-07 21:24:57 -0500

    Set this.query.itemsPerPage to totalItems when unpaged in page-factory.ts

--- src/page-factory.ts
+++ src/page-factory.ts
@@ -40,12 +40,6 @@ export class PageFactory<TEntity extends object, TOutput extends object = TEntit
     const { select, sortable, relations, where, alias } = this._config;
     let queryBuilder: QueryBuilder<TEntity> = this.isEntityRepository ? (this.repo as EntityRepository<TEntity>).createQueryBuilder(alias) : (this.repo as QueryBuilder<TEntity>);
 
-    if (this.query.unpaged) {
-      this.query.currentPage = 1;
-      this.query.offset = 0;
-      this.query.itemsPerPage = 0;
-    }
-
     if (undefined !== select) {
       queryBuilder.select(select);
     } else {
@@ -94,6 +88,10 @@ export class PageFactory<TEntity extends object, TOutput extends object = TEntit
     if (!this.query.unpaged) {
       const difference = this.query.offset + this.query.itemsPerPage - totalItems;
       queryBuilder.offset(this.query.offset).limit(this.query.itemsPerPage - (difference > 0 ? difference : 0));
+    } else {
+      this.query.currentPage = 1;
+      this.query.offset = 0;
+      this.query.itemsPerPage = totalItems;
     }
 
     const result = await queryBuilder.getResultList();
