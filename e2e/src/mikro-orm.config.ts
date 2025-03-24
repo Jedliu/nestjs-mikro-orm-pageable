f2e3bd8ee8a4c53c6321bca7818148494536a3a5
Johnny Robeson <johnny@localmomentum.net>
2025-03-24 05:45:43 -0400

    test: Set forceUtcTimeZone in mikro-orm config
    This makes it easier to work with other databases

--- e2e/src/mikro-orm.config.ts
+++ e2e/src/mikro-orm.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from '@mikro-orm/sqlite';
 export default defineConfig({
   dbName: 'e2e/src/db/test.sqlite3',
   entities: ['e2e/src/**/*.entity.ts'],
+  forceUtcTimezone: true,
   seeder: {
     path: 'e2e/src/db/seeders'
   },
